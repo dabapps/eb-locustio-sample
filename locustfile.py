3cba8dbf244b41da780076741515770ae1b8c128
Johnny Marshall <johnny@dabapps.com>
2017-01-24 10:25:03 +0000

    Trying to override index page

--- locustfile.py
+++ locustfile.py
@@ -76,7 +76,7 @@ print(dir(web.app.url_map._rules[0]))
 for rule in web.app.url_map._rules:
     print(rule.endpoint)
     if rule.endpoint == 'index':
-        web.app.url_map.remove(rule)
+        web.app.url_map._rules.remove(rule)
         print('REMOVED ORIG / RULE')
 
 
