036f36d5611a4f721f6eb893fc9a7807eea9ab75
Johnny Marshall <johnny@dabapps.com>
2017-02-02 14:17:16 +0000

    Custom filters and Excel upload

--- locustfile.py
+++ locustfile.py
@@ -365,7 +365,9 @@ class CreateSurvey(TaskSet):
         }
         print(data)
         response_2 = self.client.post(response_1.url,
-                                      name=response_1.url + (" ({} members)".format(num_team_members_to_create)),
+                                      name=create_placeholdered_url_for_stats(
+                                          response_1.url + (" ({} members)".format(num_team_members_to_create))
+                                      ),
                                       data=data,
                                       files={'excel_people_list': ('members.xlsx', create_team_member_excel_file(num_team_members_to_create))})
         print(response_2.content)
